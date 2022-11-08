7eff0393d56796da9a2fe4026d733be5dd123bc3
Quốc Thắng <dotranhaison2002@gmail.com>
2022-11-08 22:40:25 +0700

    Viet code theo phan cong
    Viet code theo phan cong

--- src/Program/ThueMotMay.hpp
+++ src/Program/ThueMotMay.hpp
@@ -5,6 +5,7 @@ class ThueMotMay : public MayTinh {
    public:
     MayTinh *maytinh = NULL;                                   // trỏ đến máy
     void chonMay(MayTinh[], int);                              // chọn máy
+    void chonMayCanThanhToan(MayTinh[], int);                  // chọn máy cần thanh toán
     void docMotNguoiThueTrucTiep(ifstream &, MayTinh[], int);  // đọc một người thuê trực tiếp từ file
     void ghiMotNguoiThueTrucTiep(ofstream &);                  // ghi một người thuê trực tiếp ra file
 };
@@ -22,6 +23,21 @@ void ThueMotMay::chonMay(MayTinh nhieuMay[], int n) {
         } else
             maytinh = layViTriCuaMay(nhieuMay, n, soMay);  // hàm này trong file NhieuMay.hpp
     } while (!kiemTraSoMay(nhieuMay, n, soMay));
+}
+
+void ThueMotMay::chonMayCanThanhToan(MayTinh nhieuMay[], int n) {
+    int soMay;
+    do {
+        system("cls");
+        xuatDanhSachCacMay(nhieuMay, n);
+        cout << "\n(?) Nhap so may: ";
+        cin >> soMay;
+        if (!kiemTraSoMayCanThanhToan(nhieuMay, n, soMay)) {
+            cout << "\n\t(!) So may khong hop le - Nhap lai (!)\n";
+            system("pause");
+        } else
+            maytinh = layViTriCuaMay(nhieuMay, n, soMay);  // hàm này trong file NhieuMay.hpp
+    } while (!kiemTraSoMayCanThanhToan(nhieuMay, n, soMay));
 }
 
 void ThueMotMay::docMotNguoiThueTrucTiep(ifstream &fileIn, MayTinh nhieuMay[], int n) {
